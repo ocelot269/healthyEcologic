a5199bbe9b29033e6303aad983b765dafd869c38
ocelot269 <ocelot269@gmail.com>
2020-05-29 00:43:01 +0200

    añadida pagina de inicio y validacion login

--- client/src/app/components/coments/coments.component.ts
+++ client/src/app/components/coments/coments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CommentService } from "../../services/comment.service";
 
 @Component({
   selector: 'app-coments',
@@ -6,12 +7,44 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./coments.component.css']
 })
 export class ComentsComponent implements OnInit {
-  @Input() idProvider = null;
+  @Input() idProduct = null;
   listComents = null;
-  constructor() { }
+  comment = null;
+  constructor(private commentService : CommentService) { }
 
   ngOnInit(): void {
-    console.log(this.idProvider);
+    console.log(this.idProduct);
+    this.listaComentarios();
+  }
+
+
+  listaComentarios(){
+     this.commentService.getListCommentsByIdProduct(this.idProduct).subscribe(
+      res => {
+        console.log(res);
+        this.listComents = res;
+      },
+      err => console.log(err)
+    );
+  }
+
+  crearComentario(){
+      let comentarioObjeto: any = {
+        id_product: this.idProduct,
+	      id_user: localStorage.getItem('idUser'),
+	      comment: this.comment
+      }
+      if (this.comment && this.idProduct && localStorage.getItem('idUser')) {
+          this.commentService.createComment(comentarioObjeto).subscribe(
+            res => {
+              this.comment= null;
+              this.listaComentarios();
+            },
+            err => console.log(err)
+        );
+      }
+      this.comment= null;
+      this.listaComentarios();
   }
 
 }
--- client/src/app/components/healthy-login/healthy-login.component.ts
+++ client/src/app/components/healthy-login/healthy-login.component.ts
@@ -5,11 +5,13 @@ import { UserService} from "../../services/user.service";
 import { Router } from '@angular/router'
 import { LoginService } from '../../services/login.service';
 import { NavigationComponent } from '../navigation/navigation.component';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-healthy-login',
   templateUrl: './healthy-login.component.html',
-  styleUrls: ['./healthy-login.component.css']
+  styleUrls: ['./healthy-login.component.css'],
+  providers: [MessageService]
 })
 export class HealthyLoginComponent implements OnInit {
 
@@ -18,7 +20,8 @@ export class HealthyLoginComponent implements OnInit {
               private userService: UserService,
               private router: Router,
               private loginService: LoginService,
-              private navigationComponent:NavigationComponent) { }
+              private navigationComponent:NavigationComponent,
+              private messageService: MessageService) { }
   ngOnInit(): void {
         this.formUsuario = this.fb.group({
           'user_name': new FormControl('', Validators.required),
@@ -30,13 +33,24 @@ export class HealthyLoginComponent implements OnInit {
       this.userService.validationUser(value).subscribe(
         res => {
           console.log(res);
-          this.loginService.setIdUser(res[0].id_user);
-          this.loginService.setLogged(true);
-          localStorage.setItem('user_type', res[0].user_type);
-          localStorage.setItem('user_name', res[0].user_name);
-          this.router.navigate(['/perfil']);
+          console.log(!res['message'].includes("no existe"));
+          if (res['message'].includes("usuario logeado")) {
+            console.log(res);
+            this.loginService.setIdUser(res['resultado']['id_user']);
+            this.loginService.setLogged(true);
+            localStorage.setItem('user_type', res['resultado']['user_type']);
+            localStorage.setItem('user_name', res['resultado']['user_name']);
+            this.router.navigate(['/']);
+          }else if (res['message'].includes("no existe")){
+              this.messageService.add({severity:'error', summary: 'Usuario no existe', detail:'Fallo al iniciar el usuario no existe'});
+          }else if (res['message'].includes("contraseña incorrecta")){
+              this.messageService.add({severity:'error', summary: 'Contraseña incorrecta', detail:'La contraseña no es correcta'});
+          }
+
         },
-        err => console.log(err)
+        err => {
+          console.log(err);
+        }
       );
   }
 
